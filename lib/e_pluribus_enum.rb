af366c604e831e2050c1714abb3bae790e7e341b
Nicholas Watson <nicholas.w.watson@me.com>
2017-07-05 15:58:45 -0400

    didnt put requires in e_pluribus_enum.rb

--- lib/e_pluribus_enum.rb
+++ lib/e_pluribus_enum.rb
@@ -1,3 +1,5 @@
 require 'e_pluribus_enum/version'
+require 'e_pluribus_enum/enum'
 require 'e_pluribus_enum/enumeration'
+require 'e_pluribus_enum/represents_enum'
 require 'e_pluribus_enum/represents_enumeration'
--- lib/e_pluribus_enum/version.rb
+++ lib/e_pluribus_enum/version.rb
@@ -1,3 +1,3 @@
 module EPluribusEnum
-  VERSION = "1.0.1"
+  VERSION = "1.0.2"
 end
